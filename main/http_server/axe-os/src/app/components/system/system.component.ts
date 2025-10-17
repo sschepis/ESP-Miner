f2ca5dcf904b7c81da64d697d77dac56c7110a46
duckAxe <210068897+duckaxe@users.noreply.github.com>
2025-10-17 19:15:57 +0200

    Fix System Page issues (#1278)

--- main/http_server/axe-os/src/app/components/system/system.component.ts
+++ main/http_server/axe-os/src/app/components/system/system.component.ts
@@ -3,6 +3,7 @@ import { Observable, Subject, combineLatest, switchMap, shareReplay, first, take
 import { SystemService } from 'src/app/services/system.service';
 import { LoadingService } from 'src/app/services/loading.service';
 import { DateAgoPipe } from 'src/app/pipes/date-ago.pipe';
+import { ByteSuffixPipe } from 'src/app/pipes/byte-suffix.pipe';
 import { ISystemInfo } from 'src/models/ISystemInfo';
 import { ISystemASIC } from 'src/models/ISystemASIC';
 
@@ -84,16 +85,15 @@ export class SystemComponent implements OnInit, OnDestroy {
     return [
       { label: 'Device Model', value: data.asic.deviceModel || 'Other', valueClass: 'text-' + data.asic.swarmColor + '-500' },
       { label: 'Board Version', value: data.info.boardVersion },
-      { label: 'Asic Type', value: (data.asic.asicCount > 1 ? data.asic.asicCount + 'x ' : ' ') + data.asic.ASICModel, class: 'pb-3' },
-
+      { label: 'ASIC Type', value: (data.asic.asicCount > 1 ? data.asic.asicCount + 'x ' : ' ') + data.asic.ASICModel, class: 'pb-3' },
       { label: 'Uptime', value: DateAgoPipe.transform(data.info.uptimeSeconds), class: 'pb-3' },
       { label: 'Wi-Fi SSID', value: data.info.ssid, isSensitiveData: true },
       { label: 'Wi-Fi Status', value: data.info.wifiStatus },
+      { label: 'Wi-Fi RSSI', value: data.info.wifiRSSI + ' dBm', valueClass: this.getWifiRssiColor(data.info.wifiRSSI), tooltip: this.getWifiRssiTooltip(data.info.wifiRSSI) },
       { label: 'Wi-Fi IPv4', value: data.info.ipv4},
-      { label: 'Wi-Fi IPv6', value: data.info.ipv6},
-      { label: 'Wi-Fi RSSI', value: data.info.wifiRSSI + ' dBm', class: 'pb-3', valueClass: this.getWifiRssiColor(data.info.wifiRSSI), tooltip: this.getWifiRssiTooltip(data.info.wifiRSSI) },
+      { label: 'Wi-Fi IPv6', value: data.info.ipv6, class: 'pb-3', isSensitiveData: true},
       { label: 'MAC Address', value: data.info.macAddr, class: 'pb-3', isSensitiveData: true },
-      { label: 'Free Heap Memory', value: data.info.freeHeap.toString(), class: 'pb-3' },
+      { label: 'Free Heap Memory', value: ByteSuffixPipe.transform(data.info.freeHeap), class: 'pb-3' },
       { label: 'Firmware Version', value: data.info.version },
       { label: 'AxeOS Version', value: data.info.axeOSVersion },
       { label: 'ESP-IDF Version', value: data.info.idfVersion },
